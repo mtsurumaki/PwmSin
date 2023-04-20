975b2ffa061a9d94824e120e63787f705ed28079
mtsurumaki <tu19980117@gmail.com>
2023-04-20 22:45:22 +0900

    example arduino project add.

--- examples/PwmSin/PwmSin.ino
+++ examples/PwmSin/PwmSin.ino
@@ -1,1 +1,30 @@
+#include <PwmSin.h>
+#define SER 13
+#define RCLK 12
+#define SRCLK 11
+#define SAMPLE 384
 
+uint8_t puttrns[SAMPLE];
+
+void setup() {
+  uint16_t i;
+
+  pinMode(SER, OUTPUT);
+  pinMode(RCLK, OUTPUT);
+  pinMode(SRCLK, OUTPUT);
+
+  PwmSin_Gen(puttrns, SAMPLE);
+
+  while(1){
+    for(i = 0; i < SAMPLE; i++){
+      digitalWrite(RCLK, LOW);
+      shiftOut(SER, SRCLK, MSBFIRST, puttrns[i]);
+      digitalWrite(RCLK, HIGH);
+      delayMicroseconds(1500);
+    }
+  }
+}
+
+void loop() {
+  /* nop */
+}
