97da539198f813c8982206e21b148f278cc356f6
mtsurumaki <tu19980117@gmail.com>
2023-04-20 22:39:19 +0900

    init commit
    PwmSin_Gen function add.

--- src/PwmSin.c
+++ src/PwmSin.c
@@ -1,1 +1,30 @@
+#include <PwmSin.h>
 
+#define TRIANGLE_CYCLE 0.02655 // sankakuha syuhasuu
+
+void PwmSin_Gen(uint8_t *array, uint16_t size)
+{
+	uint16_t i; // loop counter
+	double try;
+	double sin_var;
+	double sample;
+
+	sample = 0.0;
+	for (i = 0; i < size; i++)
+	{
+		try = ((0 <= fmod(sample, TRIANGLE_CYCLE)) && fmod(sample, TRIANGLE_CYCLE) < TRIANGLE_CYCLE / 2.0) ? (1 * (fmod(sample, TRIANGLE_CYCLE) / (TRIANGLE_CYCLE / 2.0))) : ((-1.0) * (fmod(sample, TRIANGLE_CYCLE) / (TRIANGLE_CYCLE / 2.0)) + 2.0 * 1.0);
+
+		sin_var = (sinf(2 * PI * 1 * sample) * 0.5) + 0.5;
+
+		if (sin_var >= try)
+		{
+			array[i] = 0xFF;
+		}
+		else
+		{
+			array[i] = 0x00;
+		}
+
+		sample += (1.0 / (double)size);
+	}
+}
--- src/PwmSin.h
+++ src/PwmSin.h
@@ -1,1 +1,18 @@
+#ifndef _PWM_SIN_H
+#define _PWM_SIN_H
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdint.h>
+#include <math.h>
+#include "Arduino.h"
+
+    void
+    PwmSin_Gen(uint8_t *, uint16_t);
+#ifdef __cplusplus
+}
+#endif
+#endif
