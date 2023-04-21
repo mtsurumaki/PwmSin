1e8433cd81f98c61171f6f4db3e5236cf8d84820
mtsurumaki <tu19980117@gmail.com>
2023-04-22 00:10:19 +0900

    step_value var add.

--- src/PwmSin.c
+++ src/PwmSin.c
@@ -8,8 +8,11 @@ void PwmSin_Gen(uint8_t *array, uint16_t size)
 	double try;
 	double sin_var;
 	double sample;
+	double step_value;
 
 	sample = 0.0;
+	step_value = (1.0 / (double)size);
+
 	for (i = 0; i < size; i++)
 	{
 		try = ((0 <= fmod(sample, TRIANGLE_CYCLE)) && fmod(sample, TRIANGLE_CYCLE) < TRIANGLE_CYCLE / 2.0) ? (1 * (fmod(sample, TRIANGLE_CYCLE) / (TRIANGLE_CYCLE / 2.0))) : ((-1.0) * (fmod(sample, TRIANGLE_CYCLE) / (TRIANGLE_CYCLE / 2.0)) + 2.0 * 1.0);
@@ -25,6 +28,6 @@ void PwmSin_Gen(uint8_t *array, uint16_t size)
 			array[i] = 0x00;
 		}
 
-		sample += (1.0 / (double)size);
+		sample += step_value;
 	}
 }
