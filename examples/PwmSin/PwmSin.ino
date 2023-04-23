2e7fcb2c0cdc6ed79d7c598d986e33b8d4ac274b
mtsurumaki <tu19980117@gmail.com>
2023-04-23 21:30:37 +0900

    Update PwmSin.ino

--- examples/PwmSin/PwmSin.ino
+++ examples/PwmSin/PwmSin.ino
@@ -7,24 +7,20 @@
 uint8_t puttrns[SAMPLE];
 
 void setup() {
-  uint16_t i;
-
   pinMode(SER, OUTPUT);
   pinMode(RCLK, OUTPUT);
   pinMode(SRCLK, OUTPUT);
 
   PwmSin_Gen(puttrns, SAMPLE);
-
-  while(1){
-    for(i = 0; i < SAMPLE; i++){
-      digitalWrite(RCLK, LOW);
-      shiftOut(SER, SRCLK, MSBFIRST, puttrns[i]);
-      digitalWrite(RCLK, HIGH);
-      delayMicroseconds(500);
-    }
-  }
 }
 
 void loop() {
-  /* nop */
+  uint16_t i;
+
+  for(i = 0; i < SAMPLE; i++){
+    digitalWrite(RCLK, LOW);
+    shiftOut(SER, SRCLK, MSBFIRST, puttrns[i]);
+    digitalWrite(RCLK, HIGH);
+    delayMicroseconds(500);
+  }
 }
